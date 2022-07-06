e95705680f280c3399f55ab039f9e86709879082
Jonatas Queiroz Lima <joqlima5@gmail.com>
2022-07-06 20:12:14 -0300

    add(User): model, migration and seeder

---
+++ app/backend/src/database/models/User.ts
@@ -0,0 +1,42 @@
+import { DataTypes, Model } from 'sequelize';
+import db from '.';
+
+class User extends Model {
+  id!: number;
+  username!: string;
+  role!: string;
+  email!: string;
+  password!: string; 
+}
+
+User.init({
+  id: {
+    type: DataTypes.NUMBER,    
+    allowNull: false,
+    autoIncrement: true,
+    primaryKey: true,
+  },
+  username: {
+    type: DataTypes.STRING,
+    allowNull: false,
+  },
+  role: {
+    type: DataTypes.STRING,
+    allowNull: false,
+  },
+  email: {
+    type: DataTypes.STRING,
+    allowNull: false,
+  },
+  password: {
+    type: DataTypes.STRING,
+    allowNull: false,
+  }
+}, {  
+  underscored: true,
+  sequelize: db,
+  modelName: 'Users',
+  timestamps: false,
+});
+
+export default User;
--- app/backend/src/database/seeders/20211205212238-user.js
+++ app/backend/src/database/seeders/20211205212238-user.js
@@ -1,6 +1,6 @@
 module.exports = {
   up: async (queryInterface) => {
-    await queryInterface.bulkInsert('users', [
+    await queryInterface.bulkInsert('Users', [
       {
         username: 'Admin',
         role: 'admin',
@@ -19,6 +19,6 @@ module.exports = {
   },
 
   down: async (queryInterface) => {
-    await queryInterface.bulkDelete('users', null, {});
+    await queryInterface.bulkDelete('Users', null, {});
   },
 };
