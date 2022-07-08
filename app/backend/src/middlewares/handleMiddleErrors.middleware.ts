a428fd09ab85e91524d01ab713d1a85c34f20968
Jonatas Queiroz Lima <joqlima5@gmail.com>
2022-07-07 23:34:58 -0300

    add(middleware): error
    when the error object has the key 'status', it was an error generated with { status, message }

---
+++ app/backend/src/middlewares/handleMiddleErrors.middleware.ts
@@ -0,0 +1,9 @@
+import { NextFunction, Response, Request } from "express";
+import { ErrorPayload } from "../protocols";
+
+export default function(err: ErrorPayload, _req: Request, res: Response, _next: NextFunction): Response {    
+    if (err.status) {
+        return res.status(err.status).json({ message: err.message })    
+    }
+    return res.status(500).json({ message: err.message })
+ }
