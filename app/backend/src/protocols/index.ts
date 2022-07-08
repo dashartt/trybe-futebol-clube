2121ff057fc38ed6d8b77f3a348ec48bdbee3117
Jonatas Queiroz Lima <joqlima5@gmail.com>
2022-07-07 23:31:03 -0300

    add: initial interfaces
    - User
    - ErrorPayload
    - VerifyService<T>

---
+++ app/backend/src/protocols/index.ts
@@ -0,0 +1,22 @@
+
+// ENTITIES
+export interface UserLogin {
+    email: string;
+    password: string;
+}
+
+export interface User extends UserLogin{
+    id?: number;
+    username: string;
+    role: string;    
+}
+
+// SERVICES
+export interface ErrorPayload {
+    status: number,
+    message: string,
+}
+
+export interface VerifyService<T> {
+    verifyFields(data: T, error: ErrorPayload): void | Error;
+}
