e318c9f258f392ff79b979bac77ea9aafebb4a1d
Jonatas Queiroz Lima <joqlima5@gmail.com>
2022-07-11 14:26:04 -0300

    test: add matches mock

---
+++ app/backend/src/tests/mock/index.ts
@@ -0,0 +1,104 @@
+export const match = {
+  id: 1,
+  homeTeam: 16,
+  homeTeamGoals: 1,
+  awayTeam: 8,
+  awayTeamGoals: 1,
+  inProgress: false,
+  teamHome: {
+    teamName: "São Paulo"
+  },
+  teamAway: {
+    teamName: "Grêmio"
+  }
+};
+
+export const matchArray = [
+  {
+    id: 1,
+    homeTeam: 16,
+    homeTeamGoals: 1,
+    awayTeam: 8,
+    awayTeamGoals: 1,
+    inProgress: false,
+    teamHome: {
+      teamName: "São Paulo"
+    },
+    teamAway: {
+      teamName: "Grêmio"
+    }
+  },
+  {
+    id: 42,
+    homeTeam: 6,
+    homeTeamGoals: 1,
+    awayTeam: 1,
+    awayTeamGoals: 0,
+    inProgress: true,
+    teamHome: {
+      teamName: "Ferroviária"
+    },
+    teamAway: {
+      teamName: "Avaí/Kindermann"
+    }
+  }
+]
+
+export const matchInProgressTrue = [
+  {
+    id: 41,
+    homeTeam: 16,
+    homeTeamGoals: 2,
+    awayTeam: 9,
+    awayTeamGoals: 0,
+    inProgress: true,
+    teamHome: {
+      teamName: "São Paulo"
+    },
+    teamAway: {
+      teamName: "Internacional"
+    }
+  },
+  {
+    id: 42,
+    homeTeam: 6,
+    homeTeamGoals: 1,
+    awayTeam: 1,
+    awayTeamGoals: 0,
+    inProgress: true,
+    teamHome: {
+      teamName: "Ferroviária"
+    },
+    teamAway: {
+      teamName: "Avaí/Kindermann"
+    }
+  }]
+
+export const matchInProgressFalse = [{
+  id: 3,
+  homeTeam: 4,
+  homeTeamGoals: 3,
+  awayTeam: 11,
+  awayTeamGoals: 0,
+  inProgress: false,
+  teamHome: {
+    teamName: "Corinthians"
+  },
+  teamAway: {
+    teamName: "Napoli-SC"
+  }
+},
+{
+  id: 4,
+  homeTeam: 3,
+  homeTeamGoals: 0,
+  awayTeam: 2,
+  awayTeamGoals: 0,
+  inProgress: false,
+  teamHome: {
+    teamName: "Botafogo"
+  },
+  teamAway: {
+    teamName: "Bahia"
+  }
+},]
