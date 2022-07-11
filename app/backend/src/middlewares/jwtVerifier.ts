b3388a81c6be7d5f02b5b31f3fdbfdf341a21403
Jonatas Queiroz Lima <joqlima5@gmail.com>
2022-07-11 20:15:48 -0300

    add: new middleware for jwt validation

---
+++ app/backend/src/middlewares/jwtVerifier.ts
@@ -0,0 +1,18 @@
+import { NextFunction, Response, Request } from 'express';
+import { IErrorPayload, IToken } from '../protocols';
+import AuthService from '../services/auth.service';
+
+
+export default function jwtVerifier(  
+  req: Request,
+  res: Response,
+  next: NextFunction,
+) {
+  const token = req.headers.authorization;    
+    
+  const payload = AuthService.getDataToken(token);            
+  
+  if (payload.error) { return res.status(payload.error.status).json({ message: payload.error.message }) }
+
+  next();
+}
