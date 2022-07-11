5d169b0816f48ca7e130e9f680ddc6d13230f210
Jonatas Queiroz Lima <joqlima5@gmail.com>
2022-07-11 20:17:13 -0300

    refactor: with new interfaces it was necessary to change the AuthService

--- app/backend/src/services/auth.service.ts
+++ app/backend/src/services/auth.service.ts
@@ -1,15 +1,35 @@
 import { sign, verify } from 'jsonwebtoken';
-import { IUserLogin } from '../protocols';
+import { IErrorPayload, IToken, IUserLogin } from '../protocols';
 import { config, secret } from '../configs/auth';
+import errors from '../services/errors';
+
+const { invalidToken } = errors;
 
 export default class AuthService {
+  
   static generateToken(data: Omit<IUserLogin, 'password'>) {
     return sign(data, secret, config);
   }
 
   static getDataToken(token: string | undefined) {
-    if (!token) return;
+    if (!token) {
+      return {
+        payload: null,
+        error: invalidToken as IErrorPayload,
+      }
+    }
 
-    return verify(token, secret);
+    try {      
+      const responseToken = verify(token, secret) as IToken;
+      return {
+        payload: responseToken,
+        error: null
+      }
+    } catch(error) {      
+      return {
+        payload: null,
+        error: invalidToken as IErrorPayload,
+      }
+    }
   }
 }
