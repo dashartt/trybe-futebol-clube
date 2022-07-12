998d583406d2a5f4a9125579e1e47b4ed1dfead8
Jonatas Queiroz Lima <joqlima5@gmail.com>
2022-07-12 02:28:32 -0300

    refactor: compare password with hash
    now getting the normal password by the request and comparing it with the bcrypts hash

--- app/backend/src/services/login.service.ts
+++ app/backend/src/services/login.service.ts
@@ -1,3 +1,4 @@
+import { compareSync } from 'bcryptjs';
 import User from '../database/models/User';
 import { IErrorPayload, IUserLogin } from '../protocols';
 import errors from './errors';
@@ -12,9 +13,11 @@ export default class LoginService {
   }
 
   static async isValidData({ email, password }: IUserLogin): Promise<void | IErrorPayload> {
-    const user = await User.findOne({ where: { email, password } });
+    const user = await User.findOne({ where: { email } });
 
-    if (!user) return userNotFound;
+    if (!user || !compareSync(password, user?.password)) {
+      return userNotFound;
+    }
   }
 
   static async getRole(email: string) {
