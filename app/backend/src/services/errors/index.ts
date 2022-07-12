c51d081fc3ab8ab0a378cd93c6e5fbfcaf8f4b9d
Jonatas Queiroz Lima <joqlima5@gmail.com>
2022-07-12 16:44:16 -0300

    styles(errors): fix lint

--- app/backend/src/services/errors/index.ts
+++ app/backend/src/services/errors/index.ts
@@ -1,7 +1,24 @@
 const errors = {
-  invalidLoginData: { status: 400, message: 'All fields must be filled' },
-  userNotFound: { status: 401, message: 'Incorrect email or password' },
-  invalidToken: { status: 401, message: 'Token must be a valid token' },
+  invalidLoginData: {
+    status: 400,
+    message: 'All fields must be filled',
+  },
+  userNotFound: {
+    status: 401,
+    message: 'Incorrect email or password',
+  },
+  invalidToken: {
+    status: 401,
+    message: 'Token must be a valid token',
+  },
+  invalidMatch: {
+    status: 401,
+    message: 'It is not possible to create a match with two equal teams',
+  },
+  invalidTeam: {
+    status: 404,
+    message: 'There is no team with such id!',
+  },
 };
 
 export default errors;
