27dcc1fb9aef92f4f355db4a8182c44d69eb0dee
Jonatas Queiroz Lima <joqlima5@gmail.com>
2022-07-13 16:39:00 -0300

    add: score interface

---
+++ app/backend/src/protocols/score.ts
@@ -0,0 +1,12 @@
+export default interface IScore {
+    name: string,
+    totalPoints:  number,   // P
+    totalGames: number,     // J 
+    totalVictories: number, // V
+    totalDraws: number,     // E
+    totalLosses: number,    // D
+    goalsFavor:  number,    // GP
+    goalsOwn: number,       // GC
+    goalsBalance:  number,  // SG
+    efficiency: number,     // percent
+  }
