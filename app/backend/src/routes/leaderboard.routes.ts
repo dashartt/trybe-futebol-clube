2848a7924ca10c1a190c7fb672683d9c645b33d8
Jonatas Queiroz Lima <joqlima5@gmail.com>
2022-07-13 20:01:11 -0300

    fix: leaderboard without filters
    route is: / instead of /home

--- app/backend/src/routes/leaderboard.routes.ts
+++ app/backend/src/routes/leaderboard.routes.ts
@@ -1,10 +1,11 @@
 import { Router } from 'express';
 import LeaderboardController from '../controllers/leaderboard.controller';
+
 const matchRoutes = Router();
 
 matchRoutes.get(
-  '/home',
-  LeaderboardController.getScores
+  '/',
+  LeaderboardController.getScores,
 );
 
 export default matchRoutes;
